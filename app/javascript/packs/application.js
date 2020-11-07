e7ca6fac811177b6c6a01c6b5783002ea25e741b
tamu-kazu <71427147+tamu-kazu@users.noreply.github.com>
2020-11-07 10:10:08 +0900

    天気機能の実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 require("../score.js");
 require("chartkick")
 require("chart.js")
+require("../weather.js");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
