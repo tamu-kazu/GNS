9bd68152d62f58aa25b32bfc62697a29d5301e4d
tamu-kazu <71427147+tamu-kazu@users.noreply.github.com>
2020-11-07 11:27:50 +0900

    天気、グラフ機能の削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,5 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.includes(:user)
-    @date = []
-     @posts.each do |post| 
-    @date << [post.date,post.total_score]
      end
   end
-end
