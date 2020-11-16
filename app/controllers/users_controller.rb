b046c1e4888f9229ddc130c59165581845c99169
tamu-kazu <71427147+tamu-kazu@users.noreply.github.com>
2020-11-16 22:22:31 +0900

    グラフの表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.includes(:user)
-     end
+    @data = []
+    @posts.each do |post|
+      @data << [post.date,post.total_score]
+    end
   end
+end
